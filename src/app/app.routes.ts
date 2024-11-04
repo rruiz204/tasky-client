6b4a61fcf94c5086a5ad5e4398a0366c8a36309b
roney ruiz <roneyruizrojas@gmail.com>
2024-11-03 22:21:47 -0600

    feat: create auth routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './features/auth/auth.routing';
 
-export const routes: Routes = [];
+export const routes: Routes = [...authRoutes];
